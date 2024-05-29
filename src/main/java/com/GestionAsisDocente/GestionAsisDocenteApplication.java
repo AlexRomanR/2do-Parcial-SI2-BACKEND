dc9094b68c245e3b1b82c81cf430f94859f2e4a6
AlexRomanR <thealex6969@gmail.com>
2024-05-29 15:03:21 -0400

    first commit

---
+++ src/main/java/com/GestionAsisDocente/GestionAsisDocenteApplication.java
@@ -0,0 +1,13 @@
+package com.GestionAsisDocente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAsisDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAsisDocenteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/GestionAsisDocente/dto/ReqRes.java
@@ -0,0 +1,37 @@
+package com.GestionAsisDocente.dto;
+
+
+import com.GestionAsisDocente.entity.OurUsers;
+import com.GestionAsisDocente.entity.Product;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ReqRes {
+
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String name;
+    private String email;
+    private String role;
+    private String password;
+    private List<Product> products;
+    private OurUsers ourUsers;
+    private List<OurUsers> ourUsersList;
+
+    public String getToken() {
+        return token;
+    }
+
+
+
+}
